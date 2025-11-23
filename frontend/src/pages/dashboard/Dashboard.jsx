70fb87cdf5089497d80583b69349815374d45530
Khemmie-Ray <atokemmy@gmail.com>
2025-11-23 08:34:12 +0100

    dashboard page

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DashNav, MobileDashNav } from "../../components/shared/Reuse";
+import RegisterModal from "../../components/dashboard/RegisterModal";
+import Saver from "../../components/dashboard/Saver";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <main className="">
+      <MobileDashNav>Overview</MobileDashNav>
+      <DashNav>Overview</DashNav>
+      <section className="flex justify-between my-8 lg:px-8 md:px-8 px-4 items-center flex-col lg:flex-row md:flex-row">
+        <div className="mb-3">
+          <h2 className="lg:text-[28px] md:text-[28px] text-[20px] font-semibold">
+            Hello
+          </h2>
+          <p className="text-grey">
+            Here you can manage all your vaults and contributions
+          </p>
+        </div>
+      </section>
+      <Saver />
+    </main>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
